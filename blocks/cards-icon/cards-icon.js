39cc17fea0a3246ac5b28a71f2ed44b6f42c8bcb
meejain <meetkjain83@gmail.com>
2026-01-09 01:06:10 +0530

    Fixing cards ison issues

--- blocks/cards-icon/cards-icon.js
+++ blocks/cards-icon/cards-icon.js
@@ -14,4 +14,34 @@ export default function decorate(block) {
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.replaceChildren(ul);
+
+  // Animate cards on scroll - re-trigger every time section enters/exits viewport
+  const cards = block.querySelectorAll('li');
+  let animationTimeouts = [];
+  
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const card = entry.target;
+      const index = Array.from(cards).indexOf(card);
+      
+      if (entry.isIntersecting) {
+        // Card is entering viewport - animate in with stagger
+        const timeout = setTimeout(() => {
+          card.classList.add('animate-in');
+        }, index * 150); // 150ms delay between each card
+        animationTimeouts.push(timeout);
+      } else {
+        // Card is leaving viewport - remove animation class
+        card.classList.remove('animate-in');
+        // Clear any pending timeouts for this card
+        animationTimeouts.forEach(clearTimeout);
+        animationTimeouts = [];
+      }
+    });
+  }, {
+    threshold: 0.1,
+    rootMargin: '0px 0px -50px 0px',
+  });
+
+  cards.forEach((card) => observer.observe(card));
 }
